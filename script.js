fbc65a5b9b8485a84109b00d0341569a1281bdc8
oye-nifemi <116925652+oye-nifemi@users.noreply.github.com>
2023-06-03 06:09:49 +0100

    icon - about to deploy

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
-const buttonEl = document.getElementById("button-el")
-let ulEl = document.getElementById("ul-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-buttonEl.addEventListener("click", function () {
+inputBtn.addEventListener("click", function() {
       myLeads.push(inputEl.value)
-      console.log(myLeads)
+      renderLeads()
+      inputEl.value = ""
 })
-for (let i = 0; i < myLeads.length; i++) {
-      myLeads.push(inputEl.value)
-      ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+
+
+function renderLeads() {
+      let listItems = ""
+      for (let i = 0; i < myLeads.length; i++) {
+            listItems += `
+            <li>
+                  <a target='_blank' href = https://${myLeads[i]}>
+                        ${myLeads[i]}
+                  </a> 
+            </li>
+            `
+            console.log(listItems)
+      }
+      ulEl.innerHTML = listItems
 }
